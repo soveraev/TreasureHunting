13e4287d7b792e485b67d52d1dbb3f8ca817ec71
Mathias Haudgaard <mathiashaudgaard@gmail.com>
2015-09-11 10:43:43 +0200

    setup of database with java

---
+++ app/src/main/java/app/AppConfig.java
@@ -0,0 +1,13 @@
+package app;
+package info.androidhive.loginandregistration.app;
+
+/**
+ * Created by Mathias on 11-09-2015.
+ */
+    public class AppConfig {
+        // Server user login url
+        public static String URL_LOGIN = "http://192.168.0.101/android_login_api/";
+
+        // Server user register url
+        public static String URL_REGISTER = "http://192.168.0.101/android_login_api/";
+    }
